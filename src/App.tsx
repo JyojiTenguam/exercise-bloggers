ddb823438087f6e0651ca8a157cc5c5b904c3281
JyojiTenguam <j.y.oji@hotmail.com>
2024-03-06 18:42:20 -0300

    Npm i, delete packsge.jon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import './styles/App.css';
 import { About, NotFound, Posts, Users } from './pages';
+import { Route, Routes } from 'react-router-dom';
+import Post from './components/Post';
 
 function App() {
-  return (<p>Exercício Bloggers - React Router</p>);
+  return (
+    <Routes>
+      <Route path="/" element={ <Users />}/>
+      <Route path="/posts/:id" element={ <Posts />}/>
+      <Route path="/about" element={ <About />}/>
+      <Route path="*" element={ <NotFound />}/>
+    </Routes>
+  );
 }
 
 export default App;
