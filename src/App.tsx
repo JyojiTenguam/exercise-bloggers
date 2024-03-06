8068dfeb0e65a7a766580b2970310ab823ee7a58
JyojiTenguam <j.y.oji@hotmail.com>
2024-03-06 19:24:13 -0300

    Resolvendo requisito 01 de Exercise Bloggers

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import './styles/App.css';
-import { About, NotFound, Posts, Users } from './pages';
-import { Route, Routes } from 'react-router-dom';
-import Post from './components/Post';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Users from './pages/Users';
+import Posts from './pages/Posts';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Users /> } />
-      <Route path="/posts/:id" element={ <Posts /> } />
-      <Route path="/about" element={ <About /> } />
-      <Route path="*" element={ <NotFound /> } />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Users /> } />
+        <Route path="/posts/:id" element={ <Posts /> } />
+        <Route path="/about" element={ <About /> } />
+        <Route path="*" element={ <NotFound /> } />
+      </Routes>
+    </Router>
   );
 }
 
