02a2e6c0db24b04b2e9f182fe9c6726c822be48b
JyojiTenguam <j.y.oji@hotmail.com>
2024-03-06 19:39:45 -0300

    Resolvendo requisito 02 de Exercise Bloggers

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,24 @@
 import '../styles/Header.css';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header>
       <h2>Bloggers</h2>
+      <nav>
+        <NavLink
+          to="/"
+          className="active"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className="active"
+        >
+          About
+        </NavLink>
+      </nav>
     </header>
   );
 }
